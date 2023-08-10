dbd488cd2b7a3e0cc298434086cd75d158ac1d6c
p_makwana <pmakwana@masaglobal.com>
2023-08-10 14:43:16 +0530

    minore update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -50,31 +50,18 @@ export class HomeComponent {
     tags: 'hussein Abd Elaziz',
   };
   shareLinks: { title: string; link: string }[] = [];
-  slides = [
-    { img: 'https://via.placeholder.com/600.png/09f/fff' },
-    { img: 'https://via.placeholder.com/600.png/021/fff' },
-    { img: 'https://via.placeholder.com/600.png/321/fff' },
-    { img: 'https://via.placeholder.com/600.png/422/fff' },
-    { img: 'https://via.placeholder.com/600.png/654/fff' },
-  ];
   slideConfig = { slidesToShow: 1, slidesToScroll: 1 };
   addSlide() {
-    this.slides.push({ img: 'http://placehold.it/350x150/777777' });
   }
   removeSlide() {
-    this.slides.length = this.slides.length - 1;
   }
   slickInit(e: any) {
-    console.log('slick initialized');
   }
   breakpoint(e: any) {
-    console.log('breakpoint');
   }
   afterChange(e: any) {
-    console.log('afterChange');
   }
   beforeChange(e: any) {
-    console.log('beforeChange');
   }
   constructor(public homeService: HomeService,private DomSan: DomSanitizer, private router: Router, private meta: Meta, private activatedRoute: ActivatedRoute, private snackBar: MatSnackBar,private deviceService: DeviceDetectorService) 
   { }
